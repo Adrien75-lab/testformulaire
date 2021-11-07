71b3b040453c820661e97d559a94cfbedeaa5198
Adrien75-lab <adrichristophe@gmail.com>
2021-11-07 18:48:52 +0100

    mise a jour du script pour que la value ne soit pas plus dans le formulaire de recherche

--- index.php
+++ index.php
@@ -2,29 +2,41 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Afficher/masquer une value dans une datalist</title>
-    
+    <title>Afficher/masquer une value dans une</title>
+    <style>
+      option{
+        display:none;
+      }
+    </style>
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     
+    
   </head>
   <body>
-  <input list="options" oninput="console.log(this.value);" />
-<datalist id="options">
-  <option value="1">Foo</option>
-  <option value="2">Bar</option>
-  <option value="3">Foo</option>
-</datalist>
-<script type="text/javascript">
-        document.querySelector('input').addEventListener('input', event => {
-  const value = event.target.value;
-  const opt = [].find.call(event.target.list.options, o => o.value === value);
+  <script type="text/javascript">
+        
+        const des = Object.getOwnPropertyDescriptor(HTMLInputElement.prototype, 'value');
+Object.defineProperty(HTMLInputElement.prototype, 'value', {
+  get: function() {
+    const value = des.get.call(this);
+  
+    if (this.type === 'text' && this.list) {
+      const opt = [].find.call(this.list.options, o => o.value === value);
+      return opt ? opt.dataset.value : value;
+    }
 
-  if (opt) {
-    event.target.value = opt.textContent;
-  }
+    return value;  
+  } 
 });
   
     </script>
+  <input list="options" oninput="console.log(this.value);" />
+<datalist id="options">
+<option data-value="1"><?php echo "salut"; ?></option>
+  <option data-value="2">Bar</option>
+  <option data-value="3">Foo</option>
+</datalist>
+
   </body>
 </html>
